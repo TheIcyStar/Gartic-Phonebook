01deff169005840408c2a42b7956a4a3b9d8ca21
Steven B <373152+Stevebel@users.noreply.github.com>
2022-07-23 11:59:31 -0400

    Reduce permissions

--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -11,3 +11,24 @@ if (!new class { x }().hasOwnProperty('x')) throw new Error('Transpiler is not c
 render(<Popup />, window.document.querySelector('#app-container'));
 
 if (module.hot) module.hot.accept();
+
+let injected = {};
+
+async function injectContentScript() {
+    let queryOptions = { active: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    if (tab?.id) {
+        if (injected[tab.id]) return;
+        
+        if (tab.url.startsWith("https://garticphone.com")) {
+            injected[tab.id] = true;
+            
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                files: ['contentScript.bundle.js']
+            });
+        }
+    }
+}
+
+injectContentScript();
