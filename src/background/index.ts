e5f9ef42ed46cdcbfabe2abaefbc5134ff67260e
TheIcyStar <TheIcyStar@users.noreply.github.com>
2022-07-23 15:16:45 -0400

    Change sample players

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,12 +1,12 @@
 import { PlayerData } from "../pages/Popup/players.store";
 
 const DEFAULT_PLAYERS: PlayerData[] = [{
-  username: "RubberRoss",
-  imageURL: "https://static-cdn.jtvnw.net/jtv_user_pictures/fd195f6a-5b03-41d4-9e92-e8e4019fb9f8-profile_image-300x300.png"
+  username: "Sample player",
+  imageURL: "placeholder-avatar.png"
 },
 {
-  username: "Jerma",
-  imageURL: "https://static-cdn.jtvnw.net/jtv_user_pictures/jerma985-profile_image-447425e773e6fd5c-300x300.jpeg"
+  username: "Change me!",
+  imageURL: "placeholder-avatar.png"
 }];
 
 chrome.runtime.onInstalled.addListener(() => {
